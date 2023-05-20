4520f5b6c724efe554a9faa16056acb8b828c3aa
TwentyFifthNight <128817436+TwentyFifthNight@users.noreply.github.com>
2023-05-20 23:25:36 +0200

    Add files via upload

---
+++ IS_Lab9/PHPFiles/IS_LAB9_TRANS/phantom_read/insert-adam.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "sakila2";
+$password = "pass";
+$database = "sakila";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+echo "Databse connected successfully, username " . $username . "<br><br>";
+
+$sql = "INSERT INTO actor (first_name, last_name) VALUES('ADAM','KRÓL')";
+$conn->query($sql);
+
+echo "Table actor updated";
+$conn->close();
+?>
